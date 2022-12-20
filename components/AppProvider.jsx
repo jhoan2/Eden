2fa11149d2f60243f10d3b4f88961513ebfa690a
John <johnhoang1994@gmail.com>
2022-12-20 12:22:51 -0800

    added allNotes hash map

--- components/AppProvider.jsx
+++ components/AppProvider.jsx
@@ -2,11 +2,10 @@
 import React, { createContext } from 'react'
 import { useImmerReducer } from "use-immer";
 
-
+const allNotes = new Map()
 
 const initialState = {
   changedNotes: [],
-  allNotes: new Map()
 }
 
 function AppReducer( draft, action ) {
@@ -26,19 +25,7 @@ function AppReducer( draft, action ) {
 }
 
 export const AppContext = createContext(initialState);
-// const formatJSON = (json) => {
-//   //[{type, content: []}]
-//   const id = json.content[0].attrs.id
-//   const title = json.content[0].content[0].text
-//   const note = { 
-//     id: id, 
-//     title: title,
-//     content: json,
-//   }
-//   //[{id: , title: , content: json, created_at, updated_at}]
-//   return note 
-// }
-// I could just call this here so I don't have to do this everytime. just map through and apply this function to each one.  in the state. 
+ 
 export default function AppProvider({children}) {
   const [state, dispatch] = useImmerReducer(AppReducer, initialState)
     
@@ -64,7 +51,7 @@ export default function AppProvider({children}) {
     addToChangedNotes,
     updateChangedNotes,
     changedNotes: state.changedNotes,
-    allNotes: state.allNotes
+    allNotes: allNotes
   }
 
   return (

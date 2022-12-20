3aeb728b2312b7d24d4f8f6ca3c2b9740fa8825b
John <johnhoang1994@gmail.com>
2022-12-20 12:23:25 -0800

    sidebar wip

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -1,49 +1,79 @@
 'use client'
-import React, { useState } from 'react'
-import Link from 'next/link';
-// import import useIsMounted from '../hooks/useIsMounted'
+import React, { useState, useContext, useEffect } from 'react'
+import { AppContext } from './AppProvider';
+import SideBarChanges from './SideBarChanges';
+import SideBarFolders from './SideBarFolders';
+import SearchBar from './SearchBar';
+import { connect } from "@tableland/sdk";
 
-export default function SideBar() {
+// import import useIsMounted from '../hooks/useIsMounted'
+const tableland = connect({ chain: "local-tableland" })
+
+const formatJSON = (json) => {
+  //[{changedNotes: {type: content: Array}, id: ''}]
+  const id = json.content[0].attrs.id
+  const title = json.content[0].content[0].text
+  const note = { 
+    id: id, 
+    title: title,
+    content: json,
+  }
+  //[{id: , title: , content: json, created_at, updated_at}]
+  return note 
+}
+
+const publishNotes = async (notes) => {
+    //notes is in array so map through and apply formatJSON to it. 
+    console.log(notes)
+    // const {name} = await tableland.create(`id int, title text, content text, created_at text, updated_at text`);
+    //_31337_2 
+    // await tableland.write()
+}
+
+export default function SideBar({sideBarView, setSideBarView}) {
+    const {allNotes, changedNotes} = useContext(AppContext)
     // const isMounted = useIsMounted();
     const [ toggleCollapse, setToggleCollapse ] = useState(false);
+
   return (
-    <div>
-        <div className='pl-2 pt-2'>
-            {toggleCollapse ? 
-                <button className= 'hover:bg-gray-800 rounded-md bg-gray-600 text-white' onClick={() => {setToggleCollapse(false)}}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
-                    </svg>
-                </button> : null}
-        </div>
-        <div className={`${toggleCollapse ? 'w-0 collapse' : 'w-64'} flex flex-col h-screen px-4 py-8 bg-white border-r dark:bg-gray-800 dark:border-gray-600 `}>
-            {/* <div className="relative mt-6">
-                <ConnectKitButton />
-            </div> */}
-            <div>
-                <span className='flex justify-end'>
-                    <button className='hover:bg-gray-800 rounded-md bg-gray-600 text-white' onClick={() => setToggleCollapse(true)}>
+     <div>
+        <div>
+            <div className='pl-2 pt-2'>
+                {toggleCollapse ? 
+                    <button className= 'hover:bg-gray-800 rounded-md bg-gray-600 text-white' onClick={() => {setToggleCollapse(false)}}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5" />
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
                         </svg>
-                    </button>
-                </span>            
+                    </button> : null}
             </div>
-            <div className="relative mt-6">
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                    <svg className="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
-                        <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
-                    </svg>
-                </span>
-
-                <input type="text" className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring" placeholder="Search"/>
+            <div className={`${toggleCollapse ? 'w-0 collapse' : 'w-64'} flex flex-col h-screen px-4 py-8 bg-white border-r dark:bg-gray-800 dark:border-gray-600 `}>
+                {/* <div className="relative mt-6">
+                    <ConnectKitButton />
+                </div> */}
+                <div>
+                    <span className='flex justify-end'>
+                        <button className='hover:bg-gray-800 rounded-md bg-gray-600 text-white' onClick={() => setToggleCollapse(true)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5" />
+                            </svg>
+                        </button>
+                    </span>            
+                </div>
+                <div>
+                    <div className="relative mt-6">
+                        {sideBarView ? 
+                            <SearchBar />
+                            : <button onClick={() => {publishNotes(changedNotes)}}>publish</button>
+                        }        
+                    </div>
+                    <hr className="my-6 border-gray-200 dark:border-gray-600" />
+                    <div>
+                        {sideBarView ? (<SideBarFolders />) : <SideBarChanges allNotes={allNotes} />}
+                    </div>
+                </div>
             </div>
-
-                <hr className="my-6 border-gray-200 dark:border-gray-600" />
-            </div>
-
-    </div>
-
-  )
+        </div>
+    </div>  
+    )
 }
 

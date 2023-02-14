b7071227aad2527403dee9ebb3ce7ab36d0d033c
John <johnhoang1994@gmail.com>
2023-02-14 10:31:07 -0800

    fix validDOMNesting error

--- app/notes/layout.jsx
+++ app/notes/layout.jsx
@@ -1,25 +1,17 @@
-import SideNav from "../../components/SideNav";
-// const getNotes = async() => {
-//     const res = await fetch('http://localhost:8080/chain/31337/tables/1')
-//     if(!res.ok) {
-//         throw new Error('Faild to fetch data')
-//     }
-//     return res.json()
-// }
+import SideNav from "../../components/Side/SideNav";
+
 export default async function NoteLayout({ children }) {
-    // const notes = await getNotes();
-    // const tableName = notes.name
 
     return (
 
-        <body className="flex h-screen overflow-y-hidden">
+        <section className="flex h-screen overflow-y-hidden">
                 <div>
                     <SideNav />
                 </div>
                 <div className="flex overflow-y-auto justify-center w-full">
                     {children}
                 </div>  
-        </body>
+        </section>
 
     );
   }

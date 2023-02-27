32a8dd4973e720e7bb459cd6800d0052ff62a273
John <johnhoang1994@gmail.com>
2023-02-26 17:32:43 -0800

    error page for root

--- app/error.jsx
+++ app/error.jsx
@@ -1,7 +1,7 @@
 'use client'; // Error components must be Client components
 
 import { useEffect } from 'react';
-
+import Link from 'next/link'
 export default function Error({
   error,
   reset,
@@ -13,15 +13,28 @@ export default function Error({
 
   return (
     <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+      <nav aria-label="Global">
+        <div className='p-2' aria-label="Global">
+        <Link href="/" className="flex rounded-lg p-2 w-min text-base font-semibold leading-7 text-gray-900 ring-1 bg-emerald-300 ring-gray-900/10 hover:ring-gray-900/20">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/>
+          <path d="M10.828 12l4.95 4.95-1.414 1.414L8 12l6.364-6.364 1.414 1.414z"/>
+        </svg>
+          icarus
+        </Link>
+        </div>
+      </nav>
+    <div className='flex h-screen justify-center items-center'>
+      <div>
+        <h2>Something went wrong!</h2>
+        <button
+        onClick={() => reset()}
+        className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+        >
+          Try again
+        </button>
+      </div>
     </div>
+    </div>
+
   );
 }

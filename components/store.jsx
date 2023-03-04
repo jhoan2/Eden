bc62bbb7947ef90bcb90858462bdd40189b5e86f
John <johnhoang1994@gmail.com>
2023-03-03 20:59:45 -0800

    zustand store

--- components/store.jsx
+++ components/store.jsx
@@ -7,11 +7,17 @@ export const useNoteStore = create(
     noteTreeChanged: false,
     smartAccountAddress: '',
     notes: {notes: new Map()},
+    userTableName: '',
+    devTableID: 0,
     setNoteTreeChanged: () => set({noteTreeChanged: true}),
     addFolder: (folderObject) => set((state) => {state.noteTree.push(folderObject)}),
     addPage: (pageObject) => set((state) => {state.noteTree.push(pageObject)}),
     //Auth.js
-    updateSmartAccount: (smartAccountAddress) => set({smartAccountAddress: smartAccountAddress})
+    updateSmartAccount: (smartAccountAddress) => set({smartAccountAddress: smartAccountAddress}),
+    //CreateTableButton.jsx
+    updateUserTableName: (userTableName) => set({userTableName: userTableName}),
+    //app/page.jsx
+    updateDevTableID: (devTableID) => set({devTableID: devTableID}),
     //when re-ordering, it needs to entirely reset the state. 
   }))
 )

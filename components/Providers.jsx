81f3bf57e3a39613a1ce01c4ef953776e811030a
John <johnhoang1994@gmail.com>
2023-04-05 12:14:37 -0700

    connectkitwrapper in provider

--- components/Providers.jsx
+++ components/Providers.jsx
@@ -1,13 +1,34 @@
 'use client'
 import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
+import { WagmiConfig, createClient, } from "wagmi";
+import { ConnectKitProvider, getDefaultClient } from "connectkit";
+import { polygonMumbai } from "wagmi/chains";
 
+const alchemyId = process.env.NEXT_PUBLIC_ALCHEMY_ID;
+const chains = [polygonMumbai];
+const client = createClient(
+  getDefaultClient({
+    appName: "icarus",
+    alchemyId,
+    chains,
+  }),
+);
 export default function Providers({children}) {
   const [queryClient] = React.useState(() => new QueryClient())
 
   return (
     <div>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <WagmiConfig client={client}>
+          <ConnectKitProvider>
+            {children}
+          </ConnectKitProvider>
+        </WagmiConfig>
+      </QueryClientProvider>
     </div>
   )
 }

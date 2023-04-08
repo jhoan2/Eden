acbffd13219d1c7fed5e12df4f78006e549bcc7c
John <johnhoang1994@gmail.com>
2023-04-08 15:27:20 -0700

    fixed note list item without key

--- components/Side/NoteListItem.jsx
+++ components/Side/NoteListItem.jsx
@@ -5,7 +5,7 @@ export default function NoteListItem({noteId, cid}) {
   const { user } = useNoteStore()
   
   if(!cid) return (
-    <Link href={`/user/${user.id}/${noteId}`} className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <Link href={`/user/${user.id}/${noteId}`} key={noteId} className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
       Title
     </h5>

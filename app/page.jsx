813a093fd7844411e1ace6051b12765c7f367893
John <johnhoang1994@gmail.com>
2023-05-01 14:18:27 -0700

    added loading for fetchreturninguser

--- app/page.jsx
+++ app/page.jsx
@@ -19,11 +19,11 @@ import { useAccount } from "wagmi";
 export default function Home() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const [returningUser, setReturningUser] = useState(false);
+  const [ loadingReturningUser, setLoadingReturningUser ] = useState(true)
   const isMounted = useIsMounted();
   const userTableName = useNoteStore((state) => state.user.owned_table);
   const userID = useNoteStore((state) => state.user.id);
   const { updateSmartAccount, updateUser, smartAccountAddress } = useNoteStore();
-
   const { address, isConnected } = useAccount();
 
   // const SocialLoginDynamic = dynamic(
@@ -34,8 +34,9 @@ export default function Home() {
   // );
   
   const fetchReturningUser = async (smartAccountAddress) => {
-    const db = Database.readOnly("maticmum");
-    const { results } = await db.prepare(`SELECT * FROM icarus_80001_5720 WHERE pub_address='${smartAccountAddress}';`).all();
+    const db = new Database();
+    const { results } = await db.prepare(`SELECT * FROM icarus_80001_5937 WHERE pub_address='${smartAccountAddress}';`).all();
+    setLoadingReturningUser(false)
     return results
   }
 
@@ -47,6 +48,7 @@ export default function Home() {
 
   useEffect(() => {
     if (smartAccountAddress) {
+      setLoadingReturningUser(true)
       try {
         fetchReturningUser(smartAccountAddress).then((result) => {
           if (result[0]?.pub_address === smartAccountAddress) {
@@ -58,7 +60,6 @@ export default function Home() {
         console.log({message: err})
       }
     }
-
   }, [smartAccountAddress, userTableName])
 
   return (
@@ -174,24 +175,29 @@ export default function Home() {
                 Cultivate organized thoughts and never let your ideas wither away with our app. Grow and nurture your ideas together with our collaborative note taking app.
                 Connect your wallet to get started. 
               </p>
-              
-              {smartAccountAddress ?
+
+              { smartAccountAddress ?
                 <div>
-                  {returningUser ? 
-                  <div className="mt-8 flex gap-x-4 sm:justify-center">
-                  <Link 
-                  href={`/user/${userID}`}
-                  className="inline-block rounded-lg bg-emerald-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-emerald-600 hover:bg-emerald-700 hover:ring-emerald-700"
-                  >
-                    Launch App
-                    <span className="text-emerald-200" aria-hidden="true">
-                      &rarr;
-                    </span>
-                  </Link>              
-                </div> :
-                <div className="mt-8 flex gap-x-4 sm:justify-center">
-                  <CreateTableButton /> 
-                </div>
+                  { loadingReturningUser ? 
+                    <p className='mt-8 flex gap-x-4 sm:justify-center'>Fetching user details...</p> :
+                    <div>
+                      { returningUser ? 
+                        <div className="mt-8 flex gap-x-4 sm:justify-center">
+                        <Link 
+                        href={`/user/${userID}`}
+                        className="inline-block rounded-lg bg-emerald-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-emerald-600 hover:bg-emerald-700 hover:ring-emerald-700"
+                        >
+                        Launch App
+                        <span className="text-emerald-200" aria-hidden="true">
+                          &rarr;
+                        </span>
+                        </Link>              
+                        </div> :
+                        <div className="mt-8 flex gap-x-4 sm:justify-center">
+                          <CreateTableButton /> 
+                        </div>
+                      }
+                    </div>
                   }
                 </div> :
                 null

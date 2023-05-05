539ece1d42f661129a780f38a18ff259e8b793ec
John <johnhoang1994@gmail.com>
2023-05-04 21:29:30 -0700

    fix for insecure resource

--- app/head.jsx
+++ app/head.jsx
@@ -3,6 +3,7 @@ export default function Head() {
     <>
       <title>Icarus | Personal Note Garden</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
       <meta name="description" content="Icarus, your personal note garden" />
       <link rel="icon" href="/favicon-16x16.png" />
     </>
